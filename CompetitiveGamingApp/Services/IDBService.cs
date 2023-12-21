b8d04310f1c92d5b5e831c0094d4e4a731c76359
timothy-choi <tchoi720@gmail.com>
2023-12-21 15:27:09 -0800

    adding services for postgres

---
+++ CompetitiveGamingApp/Services/IDBService.cs
@@ -0,0 +1,48 @@
+namespace CompetitiveGamingApp.Services;
+
+using Dapper;
+using Npgsql;
+using System.Data;
+using CompetitiveGamingApp.Models;
+
+
+public interface IDBService {
+    Task<T?> GetAsync<T>(string cmd, object parms);
+    Task<List<T>?> GetAll<T>(string cmd, object parms);
+
+    Task<int> EditData<T>(string cmd, object parms);
+}
+
+public class DBService : IDBService {
+    private readonly IDbConnection _db_gameHistory;
+
+    public DBService(IConfiguration configuration) {
+        _db_gameHistory = new NpgsqlConnection(configuration.GetConnectionString("PostgresSqlGameHistoryTable"));
+    }
+
+    public async Task<T?> GetAsync<T>(string cmd, object parms) {
+        T? result = default;
+        if (typeof(T) == typeof(SingleGamePaymentTransactions)) {
+            result = (await _db_gameHistory.QueryAsync<T>(cmd, parms)).FirstOrDefault();
+            if (result == null) throw new Exception("Data not found");
+        }
+        return result;
+    }
+
+    public async Task<List<T>?> GetAll<T>(string cmd, object parms) {
+        List<T>? result = default;
+        if (typeof(T) == typeof(SingleGamePaymentTransactions)) {
+            result = (await _db_gameHistory.QueryAsync<T>(cmd, parms)).ToList();
+            if (result == null) throw new Exception("Data not found");
+        }
+        return result;
+    }
+
+    public async Task<int> EditData<T>(string cmd, object parms) {
+        int result = default;
+        if (typeof(T) == typeof(SingleGamePaymentTransactions)) {
+            result = await _db_gameHistory.ExecuteAsync(cmd, parms);
+        }
+        return result;
+    }
+}

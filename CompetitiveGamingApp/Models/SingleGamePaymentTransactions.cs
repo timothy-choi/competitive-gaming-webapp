08305e039e53d2e13bdbf2a5cc9e7dc5612fdee0
timothy-choi <tchoi720@gmail.com>
2023-12-21 16:27:03 -0800

    adding game transactions controller

--- CompetitiveGamingApp/Models/SingleGamePaymentTransactions.cs
+++ CompetitiveGamingApp/Models/SingleGamePaymentTransactions.cs
@@ -6,6 +6,6 @@ public class SingleGamePaymentTransactions {
     public String? hostPlayer {get; set;}
     public String? gameId {get; set;}
     public String? playerLost {get; set;}
-    public int amountPaid {get; set;}
+    public double amountPaid {get; set;}
     public DateTime timePaid {get; set;}
 }

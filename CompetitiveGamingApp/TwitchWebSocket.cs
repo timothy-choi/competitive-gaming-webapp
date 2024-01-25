fc7c5dc3bdb3b2f033de530cf9226320e34dbf89
timothy-choi <tchoi720@gmail.com>
2024-01-24 16:57:33 -0800

    first init of twitch web socket

--- CompetitiveGamingApp/TwitchWebSocket.cs
+++ CompetitiveGamingApp/TwitchWebSocket.cs
@@ -4,24 +4,57 @@ using WebSocketSharp.Server;
 using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.SignalR;
 
+public class NotificationHub : Hub {
+    public async Task SendMessage(string msgClass, string msg) {
+        await Clients.All.SendAsync(msgClass, msg);
+    }
+}
 
 public class LiveStream : WebSocketBehavior {
-    protected override void OnMessage(MessageEventArgs e) {
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public LiveStream(IHubContext<NotificationHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+    protected async override void OnMessage(MessageEventArgs e) {
         var liveStreamRes = JObject.Parse(e.Data);
         
-
+        var notification = liveStreamRes["payload"]["subscription"];
+        if (notification != null) {
+            if (notification["type"].Equals("streams.online")) {
+                await _hubContext.Clients.All.SendAsync("streams", "Stream has started!");
+            }
+            if (notification["type"].Equals("streams.offline")) {
+                await _hubContext.Clients.All.SendAsync("streams", "Stream has ended!");
+            }
+        }
     }
 }
 
 
-public class TwitchSocket {
+public class TwitchSocket : BackgroundService {
     private WebSocketServer ws;
-    public TwitchSocket() {
+    private readonly IConfiguration _configuration;
+    private readonly IHubContext<NotificationHub> _hubContext;
+
+    public TwitchSocket(IHubContext<NotificationHub> hubContext, IConfiguration configuration) {
+        _hubContext = hubContext;
+        _configuration = configuration;
         ws = new WebSocketServer("wss://eventsub.wss.twitch.tv/ws");
-        ws.AddWebSocketService<LiveStream>("/LiveStream");
-        ws.Start();
+        ws.AddWebSocketService<LiveStream>("/LiveStream", () => new LiveStream(_hubContext));
         
-        ws.Stop();
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        ws.Start();
+        while (!stoppingToken.IsCancellationRequested) {
+            if (_configuration["WebSocketStatus"] == "Stop") {
+                ws.Stop();
+                break;
+            }
+        }
+
+        await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
     }
 }
 

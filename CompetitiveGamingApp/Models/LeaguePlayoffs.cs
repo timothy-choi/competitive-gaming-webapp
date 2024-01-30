ddb76ee37af805f37af11bcb5b25fe830ef01574
timothy-choi <tchoi720@gmail.com>
2024-01-30 11:22:03 -0800

    changed type for some playoff ds

--- CompetitiveGamingApp/Models/LeaguePlayoffs.cs
+++ CompetitiveGamingApp/Models/LeaguePlayoffs.cs
@@ -19,9 +19,9 @@ public class LeaguePlayoffs {
     public bool WholeMode {get; set;} //true = playoff selection is done as a whole; all the players will be initially determined regardless of partition, false = playoff selection is initially determined based on rank within partition
     public bool DefaultMode {get; set;} //true = playoff selection is done in the default way (all teams play in the "first round"), false = special playoff format, esp in first round (Can't use this if WholeMode is false)
     
-    public List<Tuple<int, Tuple<int, int>>>? WholeRoundOrdering {get; set;} //sets all matchups based on seed in the inital round (all teams initially play)
+    public List<Tuple<int, Tuple<String, String>>>? WholeRoundOrdering {get; set;} //sets all matchups based on seed in the inital round (all teams initially play)
     public List<PlayoffMatchup>? WholePlayoffPairings {get; set;} //list of all pairings in whole mode
-    public List<Tuple<String, List<int>>>? CombinedDivisionGroups {get; set;}
+    public List<Tuple<String, List<Tuple<int, Tuple<String, String>>>>>? CombinedDivisionGroups {get; set;}
     public List<Tuple<String, List<PlayoffMatchup>>>? CombinedDivisionPlayoffMatchups {get; set;}
     public List<Tuple<String, Tuple<int, Tuple<String, String>>>>? DivisionBasedPlayoffPairings {get; set;} //division based playoff format
     public List<Tuple<String, PlayoffMatchup>>? DivisionBasedPlayoffMatchups {get; set;}

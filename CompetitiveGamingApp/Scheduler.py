5e36342c85972959ee3fdd9b60cc1ecd9195ef0b
timothy-choi <tchoi720@gmail.com>
2024-02-03 15:36:29 -0800

    adding option to include all teams in schedule and started to work on scheduler endpoints

---
+++ CompetitiveGamingApp/Scheduler.py
@@ -0,0 +1,15 @@
+from ortools.linear_solver import pywraplp
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def SolvePlayerScheduleDivisions(num_games, groups):
+    pass
+
+def SolvePlayerScheduleWhole():
+    pass
+
+@app.route('/Schedules', methods=['POST'])
+def GeneratePlayerSchedule():
+    pass
--- CompetitiveGamingApp/Services/MongoDBService.cs
+++ CompetitiveGamingApp/Services/MongoDBService.cs
@@ -119,6 +119,7 @@ public class MongoDBService {
                 PlayerExemptLists = document.PlayerExemptLists,
                 repeatAllMatchups = document.repeatAllMatchups,
                 minRepeatMatchups = document.minRepeatMatchups,
+                playAllPlayers = document.playAllPlayers,
                 AllPartitions = document.AllPartitions,
                 AllCombinedDivisions = document.AllCombinedDivisions,
                 PlayerFullSchedule = document.PlayerFullSchedule,

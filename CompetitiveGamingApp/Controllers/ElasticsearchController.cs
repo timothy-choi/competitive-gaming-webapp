fae03c5f0c857a806eb88d2dc6dfa3189f22a6af
timothy-choi <tchoi720@gmail.com>
2024-02-14 18:08:16 -0800

    making more changes

--- CompetitiveGamingApp/Controllers/ElasticsearchController.cs
+++ CompetitiveGamingApp/Controllers/ElasticsearchController.cs
@@ -7,12 +7,14 @@ using CompetitiveGamingApp.Services;
 using Microsoft.EntityFrameworkCore;
 using Elastic.Clients.Elasticsearch;
 using StackExchange.Redis;
+using Elastic.Transport;
 
 [ApiController]
 [Route("api/Search")]
 
 public class ElasticsearchController : ControllerBase {
+    private readonly ElasticsearchClient _client;
     public ElasticsearchController() {
-        var cleint = new ElasticClient()
+        _client = new ElasticsearchClient("", new ApiKey(Environment.GetEnvironmentVariable("Elasticsearch_API_Key")!));
     }
 }

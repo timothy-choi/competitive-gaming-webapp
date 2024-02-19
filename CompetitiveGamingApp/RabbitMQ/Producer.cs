9a1c00dd8211d8dd26b46081182fce0a530ebcf7
timothy-choi <tchoi720@gmail.com>
2024-02-19 15:22:46 -0800

    impl of producer

--- CompetitiveGamingApp/RabbitMQ/Producer.cs
+++ CompetitiveGamingApp/RabbitMQ/Producer.cs
@@ -1,15 +1,12 @@
 using System.Text;
 using RabbitMQ.Client;
+using Newtonsoft.Json;
 
 
 namespace CompetitiveGamingApp.RabbitMQ;
 
 
-public interface IProducer {
-    public void SendOnboardingMessage<T>(T message);
-}
-
-public class Producer : IProducer {
+public class Producer {
     private readonly ConnectionFactory _factory;
     public Producer() {
         _factory = new ConnectionFactory {
@@ -17,6 +14,52 @@ public class Producer : IProducer {
         };
     }
 
-    public void SendMerchantCreationMessage()
+    public void SendMerchantCreationMessage<T>(T message) {
+        var connection = _factory.CreateConnection();
+
+        using var MerchantCreationChannel = connection.CreateModel();
+
+        MerchantCreationChannel.QueueDeclare("MerchantCreation", exclusive: false);
+
+        var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        MerchantCreationChannel.BasicPublish(exchange: "", routingKey: "MerchantCreation", body: data);
+    }
+
+    public void SendCustomerGrantRequestMessage<T>(T message) {
+        var connection = _factory.CreateConnection();
+
+        using var CustomerGrantRequestChannel = connection.CreateModel();
+
+        CustomerGrantRequestChannel.QueueDeclare("CustomerGrantRequest", exclusive: false);
+
+        var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        CustomerGrantRequestChannel.BasicPublish(exchange: "", routingKey: "CustomerGrantRequest", body: data);
+    }
+
+    public void SendProcessPaymentMessage<T>(T message) {
+        var connection = _factory.CreateConnection();
+
+        using var ProcessPaymentChannel = connection.CreateModel();
+
+        ProcessPaymentChannel.QueueDeclare("ProcessPayment", exclusive: false);
+
+        var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        ProcessPaymentChannel.BasicPublish(exchange: "", routingKey: "ProcessPayment", body: data);
+    }
+
+    public void SendProcessRefundMessage<T>(T message) {
+        var connection = _factory.CreateConnection();
+
+        using var ProcessRefundChannel = connection.CreateModel();
+
+        ProcessRefundChannel.QueueDeclare("ProcessRefund", exclusive: false);
+
+        var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+        ProcessRefundChannel.BasicPublish(exchange: "", routingKey: "ProcessRefund", body: data);
+    }
 
 }

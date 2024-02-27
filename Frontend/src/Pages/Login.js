acff6108da7f2981262b45ff7e5ee86893050b09
timothy-choi <tchoi720@gmail.com>
2024-02-27 15:50:43 -0800

    adding frontend login logic first

---
+++ Frontend/src/Pages/Login.js
@@ -0,0 +1,59 @@
+import { useRef, useState, useEffect, useContext } from 'react';
+import axios from './api/axios';
+import { useHistory } from 'react-router-dom'; 
+import AuthContext from "./context/AuthProvider";
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const { setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+    const history = useHistory(); 
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const loginInfo = {
+                username: username,
+                password: password
+            }
+            const response = await axios.post("/login", loginInfo);
+            setAuth({ username, password});
+
+            setUsername('');
+            setPassword('');
+            setSuccess(true);
+
+            history.push('');
+
+        } catch (err) {
+            if (!err?.response) {
+                setErrorMessage('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrorMessage('Bad Request');
+            } else if (err.response?.status === 401) {
+                setErrorMessage('Unauthorized');
+            } else {
+                setErrorMessage('Login Failed');
+            }
+            errRef.current.focus();
+        }
+    };
+
+    return ()
+
+}
+
+export default Login;

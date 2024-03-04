9f493666658f4b76443c236c6f3b097868ffea26
timothy-choi <tchoi720@gmail.com>
2024-03-04 12:38:08 -0800

    adding more to search

--- Frontend/src/Pages/SearchResults.js
+++ Frontend/src/Pages/SearchResults.js
@@ -1,10 +1,16 @@
 import { React, useRef, useState, useEffect, useContext } from 'react';
 import axios from './api/axios';
-import { useHistory } from 'react-router-dom'; 
+import { useLocation, useHistory} from 'react-router-dom'; 
 import {useAuth} from "./context/AuthProvider";
 
-const SearchResults = (query) => {
+const SearchResults = () => {
     const {loggedIn, username } = useAuth();
+
+    const location = useLocation();
+
+    const history = useHistory();
+
+    const [query, setQuery] = useState('');
 
     const [playerResults, setPlayerResults] = setState([]);
 
@@ -12,15 +18,31 @@ const SearchResults = (query) => {
 
     const [leagueResults, setLeagueResults] = setState([]);
 
-    useEffects(() => {
+    useEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const q = params.get('query');
 
-    }, []);
+        const fetchAllResults = async (q) => {
+            const playerRes = await axios.get(`/Search/Player/${q}`);
+
+            const gameRes = await axios.get(`/Search/Game/${q}`);
+
+            const leagueRes = await axios.get(`/Search/League/${q}`);
+
+        };
+
+        if (q) {
+            setQuery(q);
+
+            fetchAllResults(q);
+        }
+    }, [location.search]);
 
     const handleSearchSubmit = (event) => {
         event.preventDefault();
+
+        history.push(`/SearchResults?query=${encodeURIComponent(query)}`);
     };
-
-
 };
 
 export default SearchResults;

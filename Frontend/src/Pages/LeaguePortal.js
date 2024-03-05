f074d1f76ebc7a6967d9b8889ce5619dc0092a55
timothy-choi <tchoi720@gmail.com>
2024-03-05 01:17:01 -0800

    starting work on league portal + setup pages

--- Frontend/src/Pages/LeaguePortal.js
+++ Frontend/src/Pages/LeaguePortal.js
@@ -4,13 +4,17 @@ import { useHistory } from 'react-router-dom';
 import {useAuth} from "./context/AuthProvider";
 
 const LeaguePortal = (leagueId) => {
-    const [loggedIn] = useAuth();
+    const [loggedIn, username] = useAuth();
+
+    const [name, setName] = useState('');
 
     const [tags, setTags] = useState([]);
 
     const [description, setDescription] = useState('');
 
     const [players, setPlayers] = useState([]);
+
+    const [owner, setOwner] = useState([]);
 
     const [leagueStandings, setLeagueStandings] = useState(null);
 
@@ -44,8 +48,6 @@ const LeaguePortal = (leagueId) => {
 
     const [wholeMode, setWholeMode] = useState(false);
 
-    const [combinedDivisionMode, setCombinedDivisonMode] = useState(false);
-
     const [wholeModeOrdering, setWholeModeOrdering] = useState([]);
 
     const [divisonModeOrdering, setDivisionModeOrdering] = useState([]);
@@ -59,7 +61,77 @@ const LeaguePortal = (leagueId) => {
     const [archievePlayoffBrackets, setArchievePlayoffBrackets] = useState([]);
 
     useEffect(() => {
+        const fetchData = async () => {
+            const leagueInfo = await axios.get(`/League/${LeagueId}`);
 
+            setName(leagueInfo.data.Name);
+
+            setDescription(leagueInfo.data.Description);
+
+            setOwner(leagueInfo.data.Owner);
+
+            setTags(leagueInfo.data.Tags);
+
+            var players = [];
+
+            for (var player in leagueInfo.data.Players) {
+                var res = await axios.get(`/Player/${player[0]}`);
+                var playerInfo = {
+                    username : player[0],
+                    playerId : res.data.playerId,
+                    dateJoined : player[1]
+                };
+
+                players.push(playerInfo);
+            }
+
+            setPlayers(players);
+
+            setLeagueConfig(leagueInfo.data.LeagueConfig);
+
+            setPlayoffAssignments(leagueInfo.data.PlayoffAssignments);
+
+            setSeasonAssignments(leagueInfo.data.SeasonAssignments);
+
+            var champs = [];
+
+            var index = 0;
+
+            for (var champ in leagueInfo.data.Champions) {
+                var champInfo = {
+                    username: champ[0],
+                    playerId: champ[1],
+                    season: index + 1
+                }
+
+                champs.push(champInfo);
+
+                index++;
+            }
+
+            setChampions(champs);
+
+            setLeagueStandings(leagueInfo.data.LeagueStandings);
+
+            setArchieveLeagueStandings(leagueInfo.data.archieveLeagueStandings);
+
+            const seasonInfo = await axios.get(`/LeagueSeasonAssignments/${seasonAssignments}`);
+
+            if (seasonInfo.data.partitionsEnabled) {
+                setDivisionMode(true);
+                setDivisionStandings(leagueInfo.data.DivisionStandings);
+                setArchieveDivisionStandings(leagueInfo.data.archieveDivisionStandings);
+                if (Object.keys(seasonInfo.data.AllCombinedDivisions).length > 0) {
+                    setCombinedDivisionsMode(true);
+                    setCombinedDivisionsStandings(leagueInfo.data.combinedDivisionsStandings);
+                    setArchieveCombinedDivisionsStandings(leagueInfo.data.archieveCombinedDivisionsStandings);
+                }
+            }
+
+
+        };
+
+        fetchData();
     }, []);
 
 };

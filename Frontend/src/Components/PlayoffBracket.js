cf187b3e62f130dea56f17be746909fd4ef7ef1c
timothy-choi <tchoi720@gmail.com>
2024-03-11 22:22:14 -0700

    adding foundations for playoff bracket component

---
+++ Frontend/src/Components/PlayoffBracket.js
@@ -0,0 +1,12 @@
+import { useRef, useState, useEffect } from 'react';
+import axios from './api/axios';
+
+const PlayoffBracket = (leagueId, leaguePlayoffId) => {
+    const [playoffBracket, setPlayoffBracket] = useState(null);
+
+    useEffect(() => {
+
+    }, []);
+};
+
+export default PlayoffBracket;

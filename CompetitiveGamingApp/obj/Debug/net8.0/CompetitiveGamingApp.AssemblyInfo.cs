2bc1f8babfdeecbc8b4b60c255a1d6c811702ab9
timothy-choi <tchoi720@gmail.com>
2024-09-04 17:37:22 -0700

    added changes to deployment for sqlserver

--- CompetitiveGamingApp/obj/Debug/net8.0/CompetitiveGamingApp.AssemblyInfo.cs
+++ CompetitiveGamingApp/obj/Debug/net8.0/CompetitiveGamingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompetitiveGamingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075a434591d3d1b0701a8244bcde8ed65c7eff2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6021d0b12563bd51f0823f2ac31170691b5ab755")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompetitiveGamingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompetitiveGamingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
